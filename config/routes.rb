262f45b665316fb1b743267ff7a401166836303a
y-kazuya <y.kazuya.0124@au.com>
2018-08-15 14:40:58 +0900

    Setting user_account &flash message
    Deviseの設定とフラッシュメッセージの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "messages#index"
+  root 'messages#index'
+  resources :users, only: [:edit, :update]
 end
