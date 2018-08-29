fe3d808567a4f1285a22ba7aa2c053ee9c1cf97d
y-kazuya <y.kazuya.0124@au.com>
2018-08-29 14:03:42 +0900

    インデント修正

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,25 +1,25 @@
 $(function(){
   function buildHTML(message){
     var html = `<div class="chatscreen__main__message-lists__message">
-				  <div class="chatscreen__main__message-lists__message--user">
-				    ${message.user_name}
-				  </div>
-				  <div class="chatscreen__main__message-lists__message--date">
-					${message.created_at} 
-				  </div>
-				  <div class="chatscreen__main__message-lists__message--text">
-					${message.content}
-				  </div>`
-	
+								  <div class="chatscreen__main__message-lists__message--user">
+								    ${message.user_name}
+								  </div>
+								  <div class="chatscreen__main__message-lists__message--date">
+									  ${message.created_at}
+								  </div>
+								  <div class="chatscreen__main__message-lists__message--text">
+									  ${message.content}
+								  </div>`
+
 	if (message.image.url == null){
 	  html = $(html).append(`</div>`)
-	} 
+	}
 	else {
 	  html = $(html).append(`<div class = 'chatscreen__main__message-lists__message--image'><img src = '${message.image.url}'></div></div>`)
 	}
 	return html;
     };
-	
+
 	function scroolMessage(){
 	  var new_message = $('.chatscreen__main')[0];
 	  $(".chatscreen__main").animate({scrollTop: new_message.scrollHeight}, 'fast');
@@ -27,26 +27,26 @@ $(function(){
 
   $("#new_message").on("submit", function(e){
     e.preventDefault();
-	var formData = new FormData($(this)[0]);
-	var url = $(this).attr("action");
-	$.ajax({
-	  url: url,
-	  type: "POST",
-	  data: formData,
-	  dataType: 'json',
-	  processData: false,
-	  contentType: false
-	})
-	.done(function(data){
-	  var html = buildHTML(data);
-	  $('.chatscreen__main__message-lists').append(html);
-	  $('#new_message')[0].reset();
-	  $(".chatscreen__footer__form--send").prop("disabled", false);
-	  scroolMessage();
-	})
+		var formData = new FormData($(this)[0]);
+		var url = $(this).attr("action");
+		$.ajax({
+		  url: url,
+		  type: "POST",
+		  data: formData,
+		  dataType: 'json',
+		  processData: false,
+		  contentType: false
+		})
+		.done(function(data){
+		  var html = buildHTML(data);
+		  $('.chatscreen__main__message-lists').append(html);
+		  $('#new_message')[0].reset();
+		  $(".chatscreen__footer__form--send").prop("disabled", false);
+		  scroolMessage();
+		})
 
-	.fail(function(){
-	  alert('error');
-	});
+		.fail(function(){
+		  alert('error');
+		});
   });
 });
